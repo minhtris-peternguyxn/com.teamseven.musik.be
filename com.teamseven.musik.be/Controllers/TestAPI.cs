23729560a6f3c4bb226127b72c6fd06836c39717
minhtris-peternguyxn <minhtri10504@gmail.com>
2024-12-04 20:28:24 +0700

    Initial commit

---
+++ com.teamseven.musik.be/Controllers/TestAPI.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace com.teamseven.musik.be.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestAPI : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetHello()
+        {
+            return Ok("Hello, welcome to TestAPI!");
+        }
+    }
+}
