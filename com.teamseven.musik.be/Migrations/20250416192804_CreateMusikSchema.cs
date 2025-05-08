e6bf1c2555f762352d89d6588f0c0ac649fe87b4
minhtris-peternguyxn <minhtri10504@gmail.com>
2025-05-09 01:06:42 +0700

    fix running

---
+++ com.teamseven.musik.be/Migrations/20250416192804_CreateMusikSchema.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace com.teamseven.musik.be.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateMusikSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
